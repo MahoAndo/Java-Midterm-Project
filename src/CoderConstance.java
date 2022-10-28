094090043e8d9066c97350740f360aff9fa10932
Maho Ando <111376661+MahoAndo@users.noreply.github.com>
2022-10-27 23:50:40 -0700

    upload files

---
+++ src/CoderConstance.java
@@ -0,0 +1,28 @@
+/**
+ * @author maho ando
+ * @date Oct 23, 2022
+ *
+ */
+public class CoderConstance {
+	
+	//library information 
+	public final static String WEEKDAYS_HOURS = "Weekdays: 9:00 AM - 7:00 PM";
+	public final static String WEEKENDS_HOURS = "Weekends: 10:00 AM - 5:00 PM";
+	public final static String CLOSUER = "Last day of each month";
+	public final static String BORROW = "Book: max 10 books";
+
+	//menu option
+	public final static char INFO = '1';
+	public final static char BORROWING = '2';
+	public final static char RETURN = '3';
+	public final static char STATUS = '4';
+	public final static char SEEYOU = '5';
+	
+	//message
+	public final static String ERROR_MSG_1 = "Invaild Number.";
+	public final static String MSG_1 = "Processing has been completed.";
+	
+	//line separator
+	public final static String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+}
